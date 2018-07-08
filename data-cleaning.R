580b4e3bd5a94e93a54e532bc2863b4c516cc42d
Todd Faulkenberry <todd.faulkenberry@gmail.com>
2018-07-08 15:47:49 -0700

    updating

--- data-cleaning.R
+++ data-cleaning.R
@@ -1,13 +1,12 @@
 ## Loading packages / bringing in data ----------------------------------------
 library(tidyverse)
 library(readxl)
+library(ballr)
 
-nba_18_basic <- read_excel('data-raw/nba-basic.xlsx')
-nba_18_advanced <- read_excel('data-raw/nba-advanced.xlsx')
+nba_18_basic <- NBAPerGameStatistics(season = 2018)
+nba_18_advanced <- NBAPerGameAdvStatistics(season = 2018)
 
 ## Joining and cleaning data --------------------------------------------------
-nba_18 <- left_join(nba_18_basic, nba_18_advanced, by = c('Player', 'Pos', 'Age', 'Tm', 'G', 'MP'))
+nba_18 <- left_join(nba_18_basic, nba_18_advanced, by = c('rk', 'player', 'pos', 'age', 'tm', 'g', 'mp'))
 
-nba_18 <- nba_18 %>%
-  separate('Player', into = c('Player', 'Temp'), sep = '\\\\') %>%
-  select(-Temp)
+
